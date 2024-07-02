ffc0098f64156fd7e5a595d466430b281eb6c2c1
anjeh24 <junez24@yahoo.com>
2024-07-02 18:46:00 -0400

    initial commit, answers to part A

---
+++ jstotreacall.js
@@ -0,0 +1,14 @@
+// A. Q + A
+// How do we assign a value to a variable? We declare with either; let, const, or var followed by the variable name in camel case and then, the '=' operator
+
+// How do we change the value of a variable? We can reassign a new value to the variable that was previously declared as long as it wasn't a constant.
+
+// How do we assign an existing variable to a new variable? We declare a new variable and equate it to the existing variable.
+
+// Remind me, what are declare, assign, and define? Declare: using either let, const , or var to let js know we are about to declare a variable, assign: given a variable name, define: given a value.
+
+// What is pseudocoding and why should you do it?writing out what we want the code to achieve to give one a general picture of the task ahead and how it could be executed. It is important because it breaks down the project in an easy to execute manner.
+
+// What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it? It depends on the size and complexity of the project.
+
+
