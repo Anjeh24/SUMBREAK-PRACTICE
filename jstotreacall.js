6454370a80391c3093c01d68c58d2d2040bb5e7f
anjeh24 <junez24@yahoo.com>
2024-07-02 19:28:24 -0400

    the farm, values assessed, cow vs dog

--- jstotreacall.js
+++ jstotreacall.js
@@ -57,3 +57,17 @@ let intro = "Hello my name is" + " " + yourName;
   console.log(a != b != c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations)
   console.log(a == a != d); // note: the answer is a simple arithmetic equation, not something "weird"
   console.log(48 !== '48');
+
+
+//   D. The farm
+//   Declare a variable animal. Set it to be either "cow" or something else
+//   Write code that will print out "mooooo" if the it is equal to cow
+//   Change your code so that if the variable animalis anything other than a cow, it will print "Hey! You're not a cow."
+//   Commit
+  
+let animal = "dog";
+if (animal === "cow"){
+    console.log("mooooo");
+}else if(animal !== "cow"){
+    console.log("Hey! you're not a cow");
+}
