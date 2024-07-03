d503d141e2ef967f44b380a2018eb7bb94c21595
anjeh24 <junez24@yahoo.com>
2024-07-02 22:07:50 -0400

    what's in your closet

--- jstotreacall.js
+++ jstotreacall.js
@@ -247,5 +247,64 @@ if (numbr < 5){
 }else(console.log('monkey'));
 
 
+// H. What's in Your Closet?
+// Below, we've given you examples of Kristyn and Thom's closets modeled as data in JavaScript.
+ const kristynsCloset = [
+   "left shoe",
+   "cowboy boots",
+   "right sock",
+   "Per Scholas hoodie",
+   "green pants",
+   "yellow knit hat",
+   "marshmallow peeps"
+ ];
 
+// // Thom's closet is more complicated. Check out this nested data structure!!
+ const thomsCloset = [
+   [
+//     // These are Thom's shirts
+    "grey button-up",
+     "dark grey button-up",
+     "light blue button-up",
+     "blue button-up",
+   ],[
+//     // These are Thom's pants
+    "grey jeans",
+     "jeans",
+    "PJs"
+  ],[
+//     // Thom's accessories
+     "wool mittens",
+    "wool scarf",
+    "raybans"
+   ]
+ ];
+// What's Kristyn wearing today? Using bracket notation to access items in kristynsCloset, log the sentence "Kristyn is rocking that " + the third item in Kristyn's closet + " today!" to the console.
+// Kristyn just bought some sweet shades! Add "raybans" to her closet after "yellow knit hat".
+// Kristyn spilled coffee on her hat... modify this item to read "stained knit hat" instead of yellow.
+// Put together an outfit for Thom! Using bracket notation, access the first element in Thom's shirtsarray.
+// In the same way, access one item from Thom's pants array.
+// Access one item from Thom's accessories array.
+// Log a sentence about what Thom's wearing. Example: "Thom is looking fierce in a grey button-up, jeans and wool scarf!"
+// Get more specific about what kind of PJs Thom's wearing this winter. Modify the name of his PJ pants to Footie Pajamas.
+
+
+console.log("Kristyn is rocking that " + kristynsCloset[2] + " today!")
+
+kristynsCloset.splice(6, 0, "raybans");
+console.log(kristynsCloset);
+
+kristynsCloset[5] = "stained knit hat";
+console.log(kristynsCloset);
+
+let firstEle = thomsCloset[0][0];
+console.log(firstEle);
+let thomsPants = thomsCloset[1][1];
+console.log(thomsPants);
+let thomsAccess = thomsCloset[2][2];
+console.log(thomsAccess);
+console.log('Thom is dapper in ' + " " +firstEle + ", " + thomsPants + ' and ' + thomsAccess);
+
+thomsCloset[1][2] = "Footie Pajamas";
+console.log(thomsCloset);
 
