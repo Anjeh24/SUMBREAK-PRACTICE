689c2abba7261a299cf6c4a79632da3d7a763eb9
anjeh24 <junez24@yahoo.com>
2024-07-04 01:45:36 -0400

    changed email in object

--- jstotreacall.js
+++ jstotreacall.js
@@ -446,3 +446,27 @@ for(var i = 0; i < wordsArr.length; i++){
 }
 }
 //console.log(printOut);
+
+
+
+// Objects
+// Let's set up an object data structure. Let's say we have a website that sells products, and we have a user of our website, and we want to store that user's data. The object data structure is a good way to organize the data from our user.
+
+
+// A. Make a user object
+// Create an object called user.
+// Write in to the object the key-value pairs for name, email, age, and purchased. 
+// Set the value of purchased to an empty array []. Set the other values to whatever you would like.
+
+let user = {
+    name:"Anj",
+    email: "look@yahoo.com",
+    age: 22,
+    purchased:[]
+};
+
+// B. Update the user
+// Our user has changed his or her email address.
+//  Without changing the original user object, update the emailvalue to a new email address.
+  user.email = "looksay@yahoo.com";
+console.log(user);
